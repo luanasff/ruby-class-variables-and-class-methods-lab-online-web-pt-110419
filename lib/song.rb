ec1c3cf90b5c5476bc2bedf7193073d7a7a9d52e
Luana Faria Lane <luanasfff@gmail.com>
2019-12-05 21:30:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,3 @@
-class Song 
-  
-  attr_accessor :name, :artist ,  :genre 
-  
-    @@count = 0
-    @@artists = []
-    @@genres = []
-    @@genre_count = {}
-    @@artist_count = {}
-
-def self.count
-    @@count
-  end
-  
-  def initialize(name, artist, genre )
-    @name = name 
-    @artist = artist 
-    @genre = genre 
-   end 
 
   
-
-   
-
-end 
+ 
